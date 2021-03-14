f0aee704cd0ec26d8110ce0c584516b1903ac219
ewaldadam <ewald.adam0310@gmail.com>
2021-03-14 17:12:51 +0100

    Adding files

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 'use strict';
-const fs = require('fs');
-const bencode = require('bencode');
 
-const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
-console.log(torrent.announce.toString('utf8'));
+const tracker = require('./tracker');
+const torrentParser = require('./torrent-parser');
+
+const torrent = torrentParser.open('puppy.torrent');
+
+tracker.getPeers(torrent, peers => {
+  console.log('list of peers: ', peers);
+});
